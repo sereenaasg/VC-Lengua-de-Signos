f32ccc97738a39a649c8e25a1d836df619f6de47
Serena Sánchez García <101295075+sereenaasg@users.noreply.github.com>
2023-04-29 20:50:28 +0200

    test & train split done
    also KNN & contours approach

--- projecte.py
+++ projecte.py
@@ -16,16 +16,36 @@ from scipy.ndimage import shift
 import time
 from mpl_toolkits import mplot3d
 import math
+from sklearn.neighbors import KNeighborsClassifier
+import os
+import numpy as np
+from sklearn.model_selection import train_test_split
+from PIL import Image
+
+def carregarImatges(path):
+    imatges = []
+    etiquetes = []
+    
+    for etiqueta in os.listdir(path): ## per cada subcarpeta de la carpeta frames
+        path_total = path+etiqueta+'/' # construim el path de les imatges d'aquesta etiqueta
+        for imatge in os.listdir(path_total): # per cada imatge de la carpeta
+            path_imatge = path_total+imatge
+            imatge = Image.open(path_imatge)
+            imatges.append(np.array(imatge))
+            etiquetes.append(etiqueta)
+            
+    return np.array(imatges), etiquetes
 
 def carregarVideo(name):
-    capture = cv2.VideoCapture(name)
+    capture = cv2.VideoCapture(name+'.mp4')
     cont = 0
-    path = 'frames/'
+    path = 'frames/'+name+'/'
     
     while (capture.isOpened()):
         ret, frame = capture.read()
         if (ret == True):
-            cv2.imwrite(path + 'IMG_'+name+'_%04d.jpg' % cont, frame)    
+            if(cont%5==0):
+                cv2.imwrite(path + 'IMG_%04d.jpg' % cont, frame)    
             cont += 1
             if (cv2.waitKey(1) == ord('s')):
                 break
@@ -33,24 +53,88 @@ def carregarVideo(name):
             break
 
     capture.release()
-
-def partirDataset(datset):
-    pass
     
 
-def classificador(train, test):
-    pass
+def partirDataset(path):
+    imagenes, etiquetas = carregarImatges(path)
+    x_train, x_test, y_train, y_test = train_test_split(imagenes, etiquetas, test_size=0.2, random_state=42)
+    
+    return x_train, x_test, y_train, y_test
+    
+
+def aplanarImagenes(train, test):
+    new_train = []
+    new_test = []
+    for img in train:
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # convertir la imagen a una matriz numpy
+        img_array = np.array(img_gray)
+        # obtener la forma de la matriz numpy
+        shape = img_array.shape
+    
+        # aplanar la matriz numpy en un vector
+        img_vector = img_array.reshape(shape[0] * shape[1])
+        new_train.append(img_vector)
+        
+    for img in test:
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # convertir la imagen a una matriz numpy
+        img_array = np.array(img_gray)
+        # obtener la forma de la matriz numpy
+        shape = img_array.shape
+    
+        # aplanar la matriz numpy en un vector
+        img_vector = img_array.reshape(shape[0] * shape[1])
+        new_test.append(img_vector)
+        
+    return new_train, new_test
     
 def guardarImatge(name, img):
     skimage.io.imsave(name+'.jpg', img)
     
 
-
-
 if __name__ == "__main__":
-    names = ['video1.mp4', 'video2.mp4', 'video3.mp4']
+    names = ['subnormal', 'positivo', 'negativo']
+    path = 'frames/'
     for i in names:
-         carregarVideo(i)
+        carregarVideo(i)
+        
+    x_train, x_test, y_train, y_test = partirDataset(path)
     
+    x_train, x_test = aplanarImagenes(x_train, x_test)
     
+    # Cargar la imagen
+    # img = x_train[0]
+    
+    # # Convertir la imagen a espacio de color HSV
+    # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    # # Definir los valores de color de la piel en HSV
+    # lower_skin = np.array([20, 20, 70], dtype=np.uint8)
+    # upper_skin = np.array([140, 255, 255], dtype=np.uint8)
+    
+    # # Segmentar la imagen utilizando el umbral de color
+    # mask = cv2.inRange(hsv, lower_skin, upper_skin)
+    
+    # # Aplicar operaciones morfológicas para eliminar pequeñas áreas
+    # kernel = np.ones((5,5),np.uint8)
+    # mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    # mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    
+    # # Detectar los contornos de la región segmentada
+    # contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # # Dibujar los contornos en la imagen original
+    # plt.contour(mask, colors='r', levels=[0], alpha=0.5)
+    
+    # # Mostrar la imagen resultante
+    # plt.imshow(img)
+
+    model = KNeighborsClassifier(5)
+    model.fit(x_train, y_train)
+    predicciones = model.predict(x_test)
+    acc = model.score(x_test, y_test)
+
+
+
     
